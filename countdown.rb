0e9b1b006940391e34126ad1d4a855c4a99b2933
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-25 19:41:13 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
-end
+def countdown(int)
+  x = 0 
+  while x < 10 
+  puts "#{x} SECOND (S)!"
+  x -= 1
+  puts "HAPPY NEW YEAR"
+  end
+end 
+
+def countdown_with_sleep
+end 
